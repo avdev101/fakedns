c2241924af292b51143a84fd8eebfc1eddd6977d
aleksey <eremeev.crypt@gmail.com>
2020-09-17 14:54:22 +0300

    consum events

--- cmd/faker/main.go
+++ cmd/faker/main.go
@@ -5,8 +5,15 @@ import (
 	"github.com/eremeevdev/faker/web"
 )
 
+func consumEvents(c chan core.Event) {
+	for event := range c {
+		event.PrintLog()
+	}
+}
+
 func main() {
 	c := make(chan core.Event)
+	go consumEvents(c)
 
 	httpServer := web.NewServer(c)
 	httpServer.Start("0.0.0.0", 8080)
