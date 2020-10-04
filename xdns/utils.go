32483afe5d1128523a6bee8ddf1609c2075ca93a
aleksey <eremeev.crypt@gmail.com>
2020-10-05 00:24:11 +0300

    add get scheme func

--- xdns/utils.go
+++ xdns/utils.go
@@ -1,10 +1,29 @@
 package xdns
 
-import "regexp"
+import (
+	"regexp"
+)
 
 func getIps(s string) []string {
 
-	var pattern = regexp.MustCompile("[\\d]+\\.[\\d]+\\.[\\d]+\\.[\\d]+")
+	pattern := regexp.MustCompile("[\\d]+\\.[\\d]+\\.[\\d]+\\.[\\d]+")
 
 	return pattern.FindAllString(s, -1)
+}
+
+func getScheme(s string) []int {
+	result := make([]int, 0)
+	pattern := regexp.MustCompile("s([\\d]+)")
+	groups := pattern.FindStringSubmatch(s)
+
+	if len(groups) == 0 {
+		return result
+	}
+
+	for _, ch := range groups[1] {
+		i := int(ch - '0')
+		result = append(result, i)
+	}
+
+	return result
 }
--- xdns/utils_test.go
+++ xdns/utils_test.go
@@ -29,3 +29,31 @@ func TestGetMultiIps(t *testing.T) {
 		t.Errorf("ip not match: %v", ips[1])
 	}
 }
+
+func TestScheme(t *testing.T) {
+	scheme := getScheme("1234-s011-1234a")
+	expected := [3]int{0, 1, 1}
+
+	if len(scheme) != len(expected) {
+		t.Errorf("len mismatch")
+	}
+
+	if scheme[0] != expected[0] {
+		t.Errorf("scheme[0] mismatch")
+	}
+
+	if scheme[1] != expected[1] {
+		t.Errorf("scheme[1] mismatch")
+	}
+
+	if scheme[2] != expected[2] {
+		t.Errorf("scheme[2] mismatch")
+	}
+}
+
+func TestSchemeNoMatch(t *testing.T) {
+	scheme := getScheme("1234s")
+	if len(scheme) != 0 {
+		t.Errorf("expected len %v got %v", 0, len(scheme))
+	}
+}
